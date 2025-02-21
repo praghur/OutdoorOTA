b5e4fefec5bb1d2bd4b76437e7bd70e57bcb2074
praghur <166060957+praghur@users.noreply.github.com>
2025-02-20 19:32:42 -0500

    Update start-ue.sh

--- bin/start-ue.sh
+++ bin/start-ue.sh
@@ -1,4 +1,3 @@
-set -ux
 tmux new-session -d -s ue1
 tmux split-window -v
 tmux select-layout even-vertical
@@ -10,7 +9,7 @@ sudo ip route add 10.45.2.10 via 10.45.0.1
 
 
 
-set -ux
+
 tmux new-session -d -s ue2
 tmux split-window -v
 tmux select-layout even-vertical
@@ -21,7 +20,7 @@ sudo quectel-CM -s internet -4
 sudo ip route add 10.45.1.10 via 10.45.0.1
 
 
-set -ux
+
 tmux new-session -d -s ue3
 tmux split-window -v
 tmux select-layout even-vertical
