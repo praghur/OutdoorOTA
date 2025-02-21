628d18db612b412d90ef1d6f1621fa72760b3a97
praghur <166060957+praghur@users.noreply.github.com>
2025-02-20 19:45:00 -0500

    Update start-ue.sh

--- bin/start-ue.sh
+++ bin/start-ue.sh
@@ -5,7 +5,6 @@ tmux split-window -v
 tmux set -g mouse
 sudo quectel-CM -s internet -4
 tmux split-window -v
-sudo ip route add 10.45.0.121 via 10.45.0.1
 #Install tshark 
 sudo apt update
 sudo apt install -y tshark
@@ -17,6 +16,7 @@ tmux split-window -v
 /local/repository/bin/module-off.sh
 /local/repository/bin/module-flight.sh
 /local/repository/bin/module-on.sh
+sudo ip route add 10.45.0.121 via 10.45.0.1
 
 
 
@@ -27,7 +27,6 @@ tmux attach-session -d -t ue2
 tmux set -g mouse
 sudo quectel-CM -s internet -4
 tmux split-window -v
-sudo ip route add 10.45.3.10 via 10.45.0.1
 #Install tshark 
 sudo apt update
 sudo apt install -y tshark
@@ -39,20 +38,20 @@ tmux split-window -v
 /local/repository/bin/module-off.sh
 /local/repository/bin/module-flight.sh
 /local/repository/bin/module-on.sh
+sudo ip route add 10.45.3.10 via 10.45.0.1
 
 
 #For UE3
-tmux new-session -d -s ue2
+tmux new-session -d -s ue3
 tmux split-window -v
-tmux attach-session -d -t ue2
+tmux attach-session -d -t ue3
 tmux set -g mouse
 sudo quectel-CM -s internet -4
 tmux split-window -v
-sudo ip route add 10.45.2.10 via 10.45.0.1
 /local/repository/bin/module-off.sh
 /local/repository/bin/module-flight.sh
 /local/repository/bin/module-on.sh
-
+sudo ip route add 10.45.2.10 via 10.45.0.1
 
 #For UE4
 tmux new-session -d -s ue2
@@ -60,10 +59,19 @@ tmux split-window -v
 tmux attach-session -d -t ue2
 tmux set -g mouse
 sudo quectel-CM -s internet -4
+tmux split-window -v
+#Install tshark 
+sudo apt update
+sudo apt install -y tshark
+#Start packet capture
+sudo tshark -i enx7a6202a42b3f -T fields -e frame.time_epoch -e ip.src -e ip.dst -e ip.id -e udp.srcport -e udp.dstport -E header=y -E separator=, -E quote=d > ue2_results.csv
+tmux split-window -v
+sudo tcpdump -i enx7a6202a42b3f -w ue2_capture.pcap
 /local/repository/bin/module-off.sh
 /local/repository/bin/module-flight.sh
 /local/repository/bin/module-on.sh
 sudo ip route add 10.45.1.10 via 10.45.0.1
+
 
 
 
