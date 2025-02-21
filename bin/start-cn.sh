9a64246774710b72c965a77309f6e949b2180aac
praghur <166060957+praghur@users.noreply.github.com>
2025-02-20 19:26:44 -0500

    Update start-cn.sh

--- bin/start-cn.sh
+++ bin/start-cn.sh
@@ -1,8 +1,13 @@
-set -ux
 tmux new-session -d -s cn5g
 tmux split-window -v
-tmux select-layout even-vertical
 tmux attach-session -d -t cn5g
-sudo tc qdisc add dev ogstun root netem delay 1ms
-sudo journalctl -u open5gs-amfd -u open5gs-smfd -f --output cat
+tmux set -g mouse
+sudo journalctl -u open5gs-amfd -u open5gs-smfd -u open5gs-nrfd -u open5gs-upfd -f --output cat
 
+#Install tshark in UE1 and UE2
+sudo apt update
+sudo apt install -y tshark
+
+#Continue with packet capture
+sudo tshark -i ogstun -T fields -e frame.time_epoch -e ip.src -e ip.dst -e ip.id -e udp.srcport -e udp.dstport -E header=y -E separator=, -E quote=d > cn_results.csv
+sudo tcpdump -i ogstun -w ogstun_capture.pcap
