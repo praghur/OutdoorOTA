95d42865d83bc7793403b70468c97262dedb24f3
praghur <166060957+praghur@users.noreply.github.com>
2025-02-23 15:46:05 -0500

    Update extractlogs.sh

--- bin/extractlogs.sh
+++ bin/extractlogs.sh
@@ -9,24 +9,20 @@ output_csv2="/home/ubuntu/harq_logs.csv"
 echo "Timestamp, SINR_eq[sel]" > $output_csv1
 echo "Log Line" > $output_csv2
 
-# Read the log file line by line
 while IFS= read -r line; do
     # Check if the line contains "PUSCH"
     if [[ $line == *"PUSCH"* ]]; then
         # Extract the timestamp
         timestamp=$(echo $line | awk '{print $1}')
         
-        # Extract the sinr_eq[sel] value
-        sinr_eq=$(echo $line | grep -oP 'sinr_eq\[sel\]=\K[^\s]+')
-        
-        # Append the timestamp and sinr_eq[sel] value to the CSV file
-        echo "$timestamp, $sinr_eq" >> $output_csv1
-
-    # Check if the line contains "h_id=" and does not contain the date. This is done to reduce traffic
-    if [[ $line == *"h_id="* && $line != *"2025-"* ]]; then
-    # Append the entire line to the second CSV file
-    echo "$line" >> $output_csv2
-    fi
+        # Check if the line contains "sinr_eq[sel]"
+        if [[ $line == *"sinr_eq[sel]"* ]]; then
+            # Extract the sinr_eq[sel] value
+            sinr_eq=$(echo $line | grep -oP 'sinr_eq\[sel\]=\K[^\s]+')
+            
+            # Append the timestamp and sinr_eq[sel] value to the CSV file
+            echo "$timestamp, $sinr_eq" >> $output_csv1
+        fi
     fi
     
 done < "$log_file"
