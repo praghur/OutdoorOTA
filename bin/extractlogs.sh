e4517ca6b08c87c16bec53f1d6bd9448fa548144
praghur <166060957+praghur@users.noreply.github.com>
2025-02-23 15:36:52 -0500

    Update extractlogs.sh

--- bin/extractlogs.sh
+++ bin/extractlogs.sh
@@ -11,11 +11,16 @@ echo "Log Line" > $output_csv2
 
 # Read the log file line by line
 while IFS= read -r line; do
-    # Check if the line contains "2025-02-", "sinr_ch_est", or "sinr_eq[sel]"
-    if [[ $line == *"2025-02-"* || $line == *"sinr_ch_est"* || $line == *"sinr_eq[sel]"* ]]; then
-        # Append the entire line to the first CSV file
-        echo "$line" >> $output_csv1
-    fi
+    # Check if the line contains "PUSCH"
+    if [[ $line == *"PUSCH"* ]]; then
+        # Extract the timestamp
+        timestamp=$(echo $line | awk '{print $1}')
+        
+        # Extract the sinr_eq[sel] value
+        sinr_eq=$(echo $line | grep -oP 'sinr_eq\[sel\]=\K[^\s]+')
+        
+        # Append the timestamp and sinr_eq[sel] value to the CSV file
+        echo "$timestamp, $sinr_eq" >> $output_csv
 
     # Check if the line contains "h_id=" and does not contain the date. This is done to reduce traffic
     if [[ $line == *"h_id="* && $line != *"2025-"* ]]; then
