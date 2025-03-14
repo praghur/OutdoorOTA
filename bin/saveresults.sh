fc06380a1b168119a68e211d383c4dfa1e1716da
praghur <166060957+praghur@users.noreply.github.com>
2025-03-14 08:54:32 -0400

    Update saveresults.sh

--- bin/saveresults.sh
+++ bin/saveresults.sh
@@ -13,13 +13,13 @@ scp root@nuc1.web.powderwireless.net:ue2_capture.pcap /home/ubuntu/
 
 #Save results from UE2 - Background data
 scp root@ota-nuc2.emulab.net:ueTraff_results.csv /home/ubuntu/
-scp root@ota-nuc2.emulab.net:/local/repository/bin/ueTraff_capture.pcap /home/ubuntu/
+scp root@ota-nuc2.emulab.net:ueTraff_capture.pcap /home/ubuntu/
 
 #Save results from gnb1
-scp root@pc05-fort.emulab.net:/tmp/gnb1_mac.pcap /home/ubuntu/
-scp root@pc05-fort.emulab.net:/tmp/gnb1_ngap.pcap /home/ubuntu/
-scp root@pc05-fort.emulab.net:/tmp/gnb1-trace.log /home/ubuntu/
-scp root@pc05-fort.emulab.net:/tmp/gnb1.log /home/ubuntu/
+scp root@pc04-meb.emulab.net:/tmp/gnb1_mac.pcap /home/ubuntu/
+scp root@pc04-meb.emulab.net:/tmp/gnb1_ngap.pcap /home/ubuntu/
+scp root@pc04-meb.emulab.net:/tmp/gnb1-trace.log /home/ubuntu/
+scp root@pc04-meb.emulab.net:/tmp/gnb1.log /home/ubuntu/
 
 #Save results from gnb2
 scp root@pc01-meb.emulab.net:/tmp/gnb2_mac.pcap /home/ubuntu/
