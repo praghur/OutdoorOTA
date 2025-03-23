3373fee014853de2e09b2fd8f13224b28c834c27
praghur <166060957+praghur@users.noreply.github.com>
2025-03-23 15:31:37 -0400

    Update saveresults.sh

--- bin/saveresults.sh
+++ bin/saveresults.sh
@@ -8,7 +8,7 @@ scp root@ota-nuc1.emulab.net:ue1_capture.pcap /home/ubuntu/
 scp root@ota-nuc1.emulab.net:/local/repository/bin/traceroute_results.csv /home/ubuntu/
 
 #Save results from UE4
-scp root@nuc1.web.powderwireless.net:/local/repository/bin/ue2_results.csv /home/ubuntu/
+scp root@nuc1.web.powderwireless.net:ue2_results.csv /home/ubuntu/
 scp root@nuc1.web.powderwireless.net:ue2_capture.pcap /home/ubuntu/
 
 #Save results from UE2 - Background data
@@ -22,9 +22,6 @@ scp root@pc04-meb.emulab.net:/tmp/gnb1-trace.log /home/ubuntu/
 scp root@pc04-meb.emulab.net:/tmp/gnb1.log /home/ubuntu/
 
 #Save results from gnb2
-scp root@pc01-meb.emulab.net:/tmp/gnb2_mac.pcap /home/ubuntu/
-scp root@pc01-meb.emulab.net:/tmp/gnb2_ngap.pcap /home/ubuntu/
-scp root@pc01-meb.emulab.net:/tmp/gnb2-trace.log /home/ubuntu/
 scp root@pc01-meb.emulab.net:/var/log/gnb2.log /home/ubuntu/
 
 
