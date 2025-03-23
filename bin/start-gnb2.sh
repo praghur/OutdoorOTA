e6d8cf09ebf1c0551fb10548bb6543a707fd1dbe
praghur <166060957+praghur@users.noreply.github.com>
2025-03-23 14:18:15 -0400

    Update start-gnb2.sh

--- bin/start-gnb2.sh
+++ bin/start-gnb2.sh
@@ -1,5 +1,5 @@
-tmux new-session -d -s gnb1
+tmux new-session -d -s gnb2
 tmux split-window -v
-tmux attach-session -d -t gnb1
+tmux attach-session -d -t gnb2
 tmux set -g mouse
 sudo /var/tmp/srsRAN_Project/build/apps/gnb/gnb -c /local/repository/etc/srsran/noho.meb.yml -c /local/repository/etc/srsran/qos.yaml
